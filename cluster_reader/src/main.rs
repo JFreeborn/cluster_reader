3b0be2a2ee70d4947a3ac840159f92a565dd0c6e
Joe <joe.e.freeborn@gmail.com>
2024-04-26 06:35:25 -0500

    adding actix api

--- cluster_reader/src/main.rs
+++ cluster_reader/src/main.rs
@@ -1,32 +1,77 @@
-use actix_web::{web, App, HttpServer, Responder, HttpResponse};
-use serde::Serialize; 
-use std::io;
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::sync::Mutex;
 
-#[derive(Debug, serde::Serialize)]
-struct MyResponse {
-    message: String,
-    status: i32,
+#[get("/hello")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
 }
 
-async fn index() -> impl Responder {
-    let response = MyResponse {
-        message: "Hello, world!".to_string(),
-        status: 200,
-    };
-
-    // Return the struct as JSON using HttpResponse
-    HttpResponse::Ok().json(response)
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
 }
 
-#[tokio::main]
-async fn main() -> io::Result<()> {
-    
-    HttpServer::new(|| {
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[get("/home")]
+async fn home() -> impl Responder {
+    HttpResponse::Ok().body("heya fucko")
+}
+
+async fn about() -> impl Responder {
+    HttpResponse::Ok().body("here is the about section")
+}
+
+// This struct represents state
+struct AppState {
+    app_name: String,
+}
+
+#[get("/")]
+async fn index(data: web::Data<AppState>) -> String {
+    let app_name = &data.app_name; // <- get app_name
+    format!("Hello {app_name}!") // <- response with app_name
+}
+
+struct AppStateWithCounter {
+    counter: Mutex<i32>, // <- Mutex is necessary to mutate safely across threads
+}
+
+async fn indextwo(data: web::Data<AppStateWithCounter>) -> String {
+    let mut counter = data.counter.lock().unwrap(); // <- get counter's MutexGuard
+    *counter += 1; // <- access counter inside MutexGuard
+
+    format!("Request number: {counter}") // <- response with count
+}
+
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+
+    let counter = web::Data::new(AppStateWithCounter {
+        counter: Mutex::new(0),
+    });
+
+    HttpServer::new(move || {
         App::new()
-            .route("/api/v1/cluster-info", web::get().to(index))
+            // .app_data(web::Data::new(AppState {
+            //     app_name: String::from("Actix Web")
+            // }))
+            .app_data(counter.clone()) // <- register the created data
+            .route("/", web::get().to(indextwo))
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+            .service(home)
+            .service(
+                web::scope("/api")
+                .route("/about", web::get().to(about)))
+            .service(index)
     })
-    .bind("127.0.0.1:8080")?
+    .bind(("127.0.0.1", 8080))?
     .run()
     .await
 }
-
